d1bfd79883d74c975221a68dbf6e278451026bff
Neuru5278 <neuru5278@gmail.com>
2025-07-05 04:07:52 +0900

    refactor: Restore original dynamic dependency handling

--- Editor/Settings/AssetOrganizerSettings.cs
+++ Editor/Settings/AssetOrganizerSettings.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 using com.neuru5278.assetorganizer.Data;
 using UnityEditor.Animations;
 using UnityEngine;
-using UnityEngine.Rendering;
-using VRC.SDK3.Avatars.ScriptableObjects;
 
 namespace com.neuru5278.assetorganizer.Settings
 {
@@ -39,47 +37,45 @@ namespace com.neuru5278.assetorganizer.Settings
                 new CustomFolder("Editor")
             };
 
-            manageTypes = new ManageType[]
-            {
-                new ManageType { actionIndex = 0, name = "Animations", typeNames = new[] { typeof(AnimationClip).AssemblyQualifiedName, typeof(BlendTree).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 1, name = "Controllers", typeNames = new[] { typeof(AnimatorController).AssemblyQualifiedName, typeof(AnimatorOverrideController).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 2, name = "Textures", typeNames = new[] { typeof(Texture).AssemblyQualifiedName, typeof(Texture2D).AssemblyQualifiedName, typeof(RenderTexture).AssemblyQualifiedName, typeof(Cubemap).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 3, name = "Materials", typeNames = new[] { typeof(Material).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 4, name = "Models", typeNames = new[] { typeof(Mesh).AssemblyQualifiedName }, extensions = new[] { ".fbx", ".obj", ".dae", ".3ds", ".dxf", ".blend" } },
-                new ManageType { actionIndex = 5, name = "Prefabs", typeNames = new[] { typeof(GameObject).AssemblyQualifiedName }, extensions = new[] { ".prefab" } },
-                new ManageType { actionIndex = 6, name = "Audio", typeNames = new[] { typeof(AudioClip).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 7, name = "Masks", typeNames = new[] { typeof(AvatarMask).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 8, name = "Scenes", typeNames = new[] { typeof(UnityEditor.SceneAsset).AssemblyQualifiedName }, extensions = new[] { ".unity" } },
-                new ManageType { actionIndex = 9, name = "Presets", typeNames = new[] { typeof(UnityEditor.Presets.Preset).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 10, name = "VRC", typeNames = new[] { typeof(VRCExpressionParameters).AssemblyQualifiedName, typeof(VRCExpressionsMenu).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 11, name = "Shaders", typeNames = new[] { typeof(Shader).AssemblyQualifiedName, typeof(ComputeShader).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 12, name = "Scripts", typeNames = new[] { typeof(UnityEditor.MonoScript).AssemblyQualifiedName }, extensions = new[] { ".dll", ".cs" } },
-                new ManageType { actionIndex = 13, name = "Fonts", typeNames = new[] { typeof(Font).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 14, name = "Physics", typeNames = new[] { typeof(PhysicMaterial).AssemblyQualifiedName, typeof(PhysicsMaterial2D).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 15, name = "Lighting", typeNames = new[] { typeof(LightingDataAsset).AssemblyQualifiedName }, extensions = new string[0] },
-                new ManageType { actionIndex = 16, name = "Other", typeNames = new[] { typeof(ScriptableObject).AssemblyQualifiedName }, extensions = new string[0] }
-            };
+            var types = new List<ManageType>();
+            var actions = new List<ManageAction>();
 
-            typeActions = new ManageAction[]
+            void AddRule(string name, ManageAction action, string[] typeNames, string[] extensions = null)
             {
-                ManageAction.Copy, // 0: Animations
-                ManageAction.Copy, // 1: Controllers  
-                ManageAction.Copy, // 2: Textures
-                ManageAction.Copy, // 3: Materials
-                ManageAction.Copy, // 4: Models
-                ManageAction.Copy, // 5: Prefabs
-                ManageAction.Copy, // 6: Audio
-                ManageAction.Copy, // 7: Masks
-                ManageAction.Copy, // 8: Scenes
-                ManageAction.Skip, // 9: Presets
-                ManageAction.Copy, // 10: VRC
-                ManageAction.Skip, // 11: Shaders
-                ManageAction.Skip, // 12: Scripts
-                ManageAction.Copy, // 13: Fonts
-                ManageAction.Copy, // 14: Physics
-                ManageAction.Copy, // 15: Lighting
-                ManageAction.Copy, // 16: Other
-            };
+                types.Add(new ManageType {
+                    actionIndex = actions.Count,
+                    name = name,
+                    typeNames = typeNames,
+                    extensions = extensions ?? new string[0]
+                });
+                actions.Add(action);
+            }
+
+            // Define all rules dynamically
+            AddRule("Animations", ManageAction.Copy, new[] { typeof(AnimationClip).AssemblyQualifiedName, typeof(BlendTree).AssemblyQualifiedName });
+            AddRule("Controllers", ManageAction.Copy, new[] { typeof(AnimatorController).AssemblyQualifiedName, typeof(AnimatorOverrideController).AssemblyQualifiedName });
+            AddRule("Textures", ManageAction.Copy, new[] { typeof(Texture).AssemblyQualifiedName, typeof(Texture2D).AssemblyQualifiedName, typeof(RenderTexture).AssemblyQualifiedName, typeof(Cubemap).AssemblyQualifiedName });
+            AddRule("Materials", ManageAction.Copy, new[] { typeof(Material).AssemblyQualifiedName });
+            AddRule("Models", ManageAction.Copy, new[] { typeof(Mesh).AssemblyQualifiedName }, new[] { ".fbx", ".obj", ".dae", ".3ds", ".dxf", ".blend" });
+            AddRule("Prefabs", ManageAction.Copy, new[] { typeof(GameObject).AssemblyQualifiedName }, new[] { ".prefab" });
+            AddRule("Audio", ManageAction.Copy, new[] { typeof(AudioClip).AssemblyQualifiedName });
+            AddRule("Masks", ManageAction.Copy, new[] { typeof(AvatarMask).AssemblyQualifiedName });
+            AddRule("Scenes", ManageAction.Copy, new[] { typeof(UnityEditor.SceneAsset).AssemblyQualifiedName }, new[] { ".unity" });
+            AddRule("Presets", ManageAction.Skip, new[] { typeof(UnityEditor.Presets.Preset).AssemblyQualifiedName });
+            AddRule("VRC", ManageAction.Copy, new[] { 
+                "VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionParameters, VRCSDK3A", 
+                "VRC.SDK3.Avatars.ScriptableObjects.VRCExpressionsMenu, VRCSDK3A" 
+            });
+            AddRule("Shaders", ManageAction.Skip, new[] { typeof(Shader).AssemblyQualifiedName, typeof(ComputeShader).AssemblyQualifiedName });
+            AddRule("Scripts", ManageAction.Skip, new[] { typeof(UnityEditor.MonoScript).AssemblyQualifiedName }, new[] { ".dll", ".cs" });
+            AddRule("Fonts", ManageAction.Copy, new[] { typeof(Font).AssemblyQualifiedName });
+            AddRule("Physics", ManageAction.Copy, new[] { typeof(PhysicMaterial).AssemblyQualifiedName, typeof(PhysicsMaterial2D).AssemblyQualifiedName });
+
+            AddRule("Other", ManageAction.Copy, new[] { typeof(ScriptableObject).AssemblyQualifiedName });
+
+            // Assign the generated lists back to the arrays
+            manageTypes = types.ToArray();
+            typeActions = actions.ToArray();
         }
     }
 } 
